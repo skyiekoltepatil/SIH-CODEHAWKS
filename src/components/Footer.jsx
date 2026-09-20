abef798cb48c3047283e218a0abfabbad9651a3e
skyie@ <bhushankolte20@gmail.com>
2026-09-20 06:06:05 +0530

    Add dashboard link to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,6 +16,7 @@ export default function Footer() {
           <h3>Quick Links</h3>
           <ul>
             <li><Link to="/">Home</Link></li>
+            <li><Link to="/dashboard">Dashboard</Link></li>
             <li><Link to="/about">About Us</Link></li>
             <li><Link to="/schemes">Schemes</Link></li>
             <li><Link to="/services">Services</Link></li>
